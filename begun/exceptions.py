499833e8cf18f39c58b277add650f255672320cd
Boris Bobrov <breton@cynicmansion.ru>
2014-02-14 10:10:31 +0500

    basic class and exceptions, some changes to gitignore

---
+++ begun/exceptions.py
@@ -0,0 +1,21 @@
+import requests
+
+class BaseBegunError(Exception):
+    pass
+
+class RequestException(BaseBegunError):
+    '''Is raised when requests.exceptions.RequestException happens'''
+    def __init__(self, request_exception):
+        self.request_exception = request_exception
+        super(RequestException, self).__init__(request_exception)
+
+class CallError(BaseBegunError):
+    '''Is raised when ``error`` key is returned by Begun'''
+    def __init__(self, error):
+        self.error = error
+        m = u'{0}: {1}'.format(error['error_code'], error['error_message'])
+        super(RequestException, self).__init__(m)
+
+class BadResponse(BaseBegunError):
+    '''sometimes Begun returns not json. Shit happens'''
+    pass
